6855b59342a7adc718af2a91bd7e18184c59f88e
Junyan Li <lijunyan668@outlook.com>
2021-11-06 17:47:27 +0800

    add scripts

---
+++ han3.sh
@@ -0,0 +1,1 @@
+python -m torch.distributed.launch --nproc_per_node=2 --nnodes=4 --node_rank=3 --master_addr="10.93.89.24" --master_port=20810 train.py --data data/widerface.yaml --cfg models/yolov5m.yaml --weights weights/yolov5m.pt --sync-bn --batch-size 128 --cache-images --multi-scale --single-cls --log-imgs 4
